c14410e49e5e04f34a0046c7196884098cc17d91
nikmorror37 <nikitamironchuk37@gmail.com>
2023-02-27 20:52:49 +0300

    Initial commit

---
+++ HomeTask9DopZadanie/HomeTask9DopZadanie/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace HomeTask9DopZadanie
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
